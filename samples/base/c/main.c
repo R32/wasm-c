5afe04094cc32dd0f56b3cf3711c58bfe746ff8e
r32 <56782055@qq.com>
2021-05-04 17:15:30 +0800

    [misc] updated samples/base

--- samples/base/c/main.c
+++ samples/base/c/main.c
@@ -1,17 +1,24 @@
-#include "stdint.h"
-#include "limits.h"
-#include "ctype.h"
 #include "WASM.h"
+
+// the builtin variables & function
+extern const int __memory_base;
+extern const int __global_base;
+extern const int __data_end;
+extern const int __heap_base;
+extern const int __table_base;
+extern const int __dso_handle;
+void __wasm_call_ctors();
 
 // export by "wasm-ld --export=square" in Makefile
 int square(int n) {
 	return n * n;
 }
-// export by macro
+
 EM_IMPORT(log) void log(int a, int b, int c);
 
+// export by macro
 EM_EXPORT(test) int arbitrary_name(int c) {
 	int stacksize = &__heap_base - &__data_end;
 	log((int)&__data_end, (int)&__heap_base, stacksize);
-	return isalpha(c); // ctype.c
+	return c;
 }
