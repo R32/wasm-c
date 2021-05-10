8e4bbb5cc6f1b98afdb4f37238ad57c2344fc056
r32 <56782055@qq.com>
2021-05-11 02:35:29 +0800

    updated samples/libc

--- samples/libc/c/main.c
+++ samples/libc/c/main.c
@@ -8,6 +8,8 @@
 #include "_malloc.h"
 #include "math.h"
 #include "string.h"
+#include "wchar.h"
+#include "stdlib.h"
 #include "WASM.h"
 
 extern const int __heap_base;
@@ -15,7 +17,7 @@ extern const int __data_end;
 extern const int __global_base;
 
 EM_EXPORT(test) double arbitrary_name(double v) {
-	printf("-%s %ls-", "hello", L"世界");
+	printf("Only supports ansi characters!\n");
 	printf("global_base: %d, data_end: %d, heap_base: %d, stack size: %d\n",
 		(int)&__global_base,
 		(int)&__data_end,
@@ -24,5 +26,6 @@ EM_EXPORT(test) double arbitrary_name(double v) {
 			? (int)&__global_base
 			: (int)&__heap_base - (int)&__data_end
 	);
+	printf("wcslen: %d\n", wcslen(L"年年岁岁花相似"));
 	return sqrt(v);
 }
