01bbf855c8edbd8dc4170fcfdce0fe90fd30b02a
r32 <56782055@qq.com>
2021-05-10 20:47:49 +0800

    added empty stdio.h, stdbool.h

---
+++ include/stdbool.h
@@ -0,0 +1,12 @@
+/*
+* SPDX-License-Identifier: GPL-2.0
+*/
+
+#ifndef _STDBOOL_H
+#define _STDBOOL_H    1
+
+#define true     1
+#define false    0
+typedef _Bool    bool;
+
+#endif
---
+++ include/stdio.h
@@ -0,0 +1,12 @@
+/*
+* SPDX-License-Identifier: GPL-2.0
+*/
+
+#ifndef _STDIO_H
+#define _STDIO_H    1
+#include "_cdefs.h"
+
+C_FUNCTION_BEGIN
+
+C_FUNCTION_END
+#endif
